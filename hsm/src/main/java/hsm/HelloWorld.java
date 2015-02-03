ef3b38ade9b83a24ea6e3123c90e2a781d38bcc1
sinan <sinangoo@gmx.de>
2015-02-03 13:28:13 +0100

    setup basic junit project structure

---
+++ hsm/src/main/java/hsm/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.hsm;
+
+public class HelloWorld {
+
+    public String hello() {
+        return "Hello World";
+    }
+
+}
---
+++ tests/src/test/java/hsm/HelloWorldTest.java
@@ -0,0 +1,16 @@
+package com.hsm;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+
+import com.hsm.HelloWorld;
+
+public class HelloWorldTest {
+
+    @Test
+    public void testResponse() {
+        assertThat(new HelloWorld().hello(), equalTo("Hello World"));
+    }
+
+}
